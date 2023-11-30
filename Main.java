f903f5c7de0628d5058fb10019524005201af0f1
Ahmad <mahmedraza90@gmail.com>
2023-11-30 06:08:07 +0000

    Initial draft

---
+++ Main.java
@@ -0,0 +1,25 @@
+class Main{
+    public static void main(String[] args){
+
+    }
+}
+class Node{
+    Node next;
+    Node prev;
+    int data;
+
+    Node(){
+        next = null;
+        prev = null;
+        data = 0;
+    }
+    Node(int value){
+        next = null;
+        prev = null;
+        data = value;
+    }
+}
+class Tree{
+    Node root;
+    
+}
